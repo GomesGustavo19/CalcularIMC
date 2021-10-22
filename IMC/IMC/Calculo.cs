b33c50850e46d36fef81587fc34393252ce8a53e
GomesGustavo19 <gugags2@hotmail.com>
2021-10-21 23:58:47 -0300

    Primeira versão

---
+++ IMC/IMC/Calculo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMC
+{
+    public class Calculo
+    {
+
+        public string Nome { get; set; }
+        public double Peso  { get; set; }
+        public double Altura { get; set; }
+        public double Resultado { get; set; }
+
+        public void Calcular()
+        {
+
+            Console.WriteLine("Qual o seu nome:");
+            Nome = Console.ReadLine();
+            Console.WriteLine("Qual e o seu Peso: (xx,xx)");
+            Peso = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Qual sua Altura: (x,xx)");
+            Altura = double.Parse(Console.ReadLine());
+
+
+            Resultado = Math.Round(Peso /(Altura * Altura));
+
+            Console.Write(Nome + "O seu IMC é: " + Resultado);
+        }
+    }
+}
---
+++ IMC/IMC/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculo calc = new Calculo();
+
+            calc.Calcular();
+        }
+    }
+}
